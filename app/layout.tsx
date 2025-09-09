1739009b677019337130a0f918117af448cc280e
Souraevshing <isauravshing@gmail.com>
2025-09-09 12:59:58 +0530

    config: add theme provider and wrap the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import React from "react";
+import {ThemeProvider} from "@/lib/theme-provider";
 
 import "./globals.css";
 
@@ -29,9 +30,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen bg-white text-neutral-900`}
       >
+      <ThemeProvider>
       <div className={`container mx-auto max-w-2xl px-4 py-8`}>
           {children}
       </div>
+      </ThemeProvider>
       </body>
     </html>
   );
