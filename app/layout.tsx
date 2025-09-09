22e7d2b1b09815729098cd3bbda73fd9b0b3b19e
Souraevshing <isauravshing@gmail.com>
2025-09-09 18:12:17 +0530

    feat: add configuration for Toaster

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import React from "react";
 import {Toaster} from "sonner";
+import {BanIcon, CheckCircle2Icon, InfoIcon, Loader2Icon, ShieldAlertIcon, XIcon} from "lucide-react";
 
 import {ThemeProvider} from "@/lib/theme-provider";
 import Navbar from "@/components/navbar";
@@ -37,7 +38,7 @@ export default function RootLayout({
           <Navbar />
           <main className="container mx-auto max-w-2xl px-4 py-6">{children}</main>
           <Footer/>
-          <Toaster richColors position="top-center" />
+          <Toaster richColors position="top-center" visibleToasts={2} expand duration={1500} toastOptions={{duration:1500}} icons={{success: <CheckCircle2Icon className={`w-4 h-4 text-green-500`}/>, info: <InfoIcon className={`w-4 h-4 text-yellow-500`} />, close: <XIcon className={`w-4 h-4 text-red-500`}/>, loading: <Loader2Icon className={`w-4 h-4`}/>, error: <BanIcon className={`w-4 h-4 text-red-500`}/>, warning: <ShieldAlertIcon className={`w-4 h-4 text-red-500`}/>}} />
       </ThemeProvider>
       </body>
     </html>
